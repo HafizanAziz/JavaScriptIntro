87dc100712179ad02350bd000381d8465c0aa874
Hafizan <mohamad.hafizan@kloudious.com>
2024-07-19 11:17:42 +0800

    commit

---
+++ function.js
@@ -0,0 +1,29 @@
+function add(a,b)
+{
+    return a+b
+}
+
+function subtract(a,b)
+{
+    return a-b
+}
+
+function multi(a,b)
+{
+    return a*b
+}
+
+function devide(a,b)
+{
+    return a/b
+}
+
+// let total1=add(10,3)
+// let total2=subtract(10,3)
+// let total3=multi(10,3)
+// let total4=devide(10,3)
+
+console.log(add(10,3))
+console.log(subtract(10,3))
+console.log(multi(10,3))
+console.log(devide(10,3))
