64a99746d13d32770d1bcfc73a38da3d46e925ab
Hafizan <mohamad.hafizan@kloudious.com>
2024-07-25 12:44:02 +0800

    commit

--- object.js
+++ object.js
@@ -16,3 +16,4 @@ console.log(person) //{ FirstName: 'John', LastName: 'Wick', Gender: 'Male' }
 delete person.LastName // delete LastName property from person Object
 console.log(person) // { FirstName: 'John', Gender: 'Male' }
 
+console.log('LastName' in person)
