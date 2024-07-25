8b27d603491f42d6f8d6509cd2d5ab12eba539ea
Hafizan <mohamad.hafizan@kloudious.com>
2024-07-25 12:40:21 +0800

    commit

---
+++ object.js
@@ -0,0 +1,15 @@
+let person = 
+{
+    FirstName:'John',
+    LastName:'Wick'
+
+}
+
+console.log(person) // { FirstName: 'John', LastName: 'Wick' }
+console.log(person.LastName) // Wick
+console.log(person.FirstName) //John
+
+person.Gender='Male' // add Gender:Male property to person object
+
+console.log(person) //{ FirstName: 'John', LastName: 'Wick', Gender: 'Male' }
+
