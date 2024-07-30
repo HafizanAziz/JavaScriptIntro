3095567b0875ae09a85595bbae7061cf548ec8b0
Hafizan <mohamad.hafizan@kloudious.com>
2024-07-30 15:15:27 +0800

    commit

--- OOPS.js
+++ OOPS.js
@@ -1,17 +1,28 @@
+// Define the Kapal class
 class Kapal {
+    // Constructor to initialize age and color properties
     constructor() {
-        this.age = 31;
-        this.color = 'Biru';
+        this.age = 31;          // Set default age to 31
+        this.color = 'Biru';    // Set default color to 'Biru'
     }
 
+    // Getter method for the location property
     get location() {
-        return "Canada";
+        return "Canada";        // Always returns 'Canada' as the location
     }
 }
 
+// Export the Kapal class
+module.exports = Kapal;
+
+// Create an instance of the Kapal class
 let kapal = new Kapal();
-console.log(kapal.age);
-console.log(kapal.location);
-console.log(kapal.color);
 
+// Log the age property of the kapal instance
+console.log(kapal.age);        // Outputs: 31
 
+// Log the location property of the kapal instance
+console.log(kapal.location);   // Outputs: Canada
+
+// Log the color property of the kapal instance
+console.log(kapal.color);      // Outputs: Biru
--- import.js
+++ import.js
@@ -1,3 +1,14 @@
-//Export Output
-const Kapal =  require('./OOPS')
+// Import the Kapal class from the 'OOPS' module
+const Kapal = require('./OOPS');
 
+// Create an instance of the Kapal class
+let kapal = new Kapal();
+
+// Log the age property of the kapal instance
+console.log(kapal.age);        // Outputs: 31
+
+// Log the location property of the kapal instance
+console.log(kapal.location);   // Outputs: Canada
+
+// Log the color property of the kapal instance
+console.log(kapal.color);      // Outputs: Biru
